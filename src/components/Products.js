d0ca6e8d9d8f80fd758b11af6f3485daedbe49da
Logambigan Thiruganeshapathy <logambiganthiruganeshapathy@gmail.com>
2024-12-03 18:24:40 +0530

    setup react project

---
+++ src/components/Products.js
@@ -0,0 +1,30 @@
+
+import '../assets/CSS/layout.css';
+import Product from './Product';
+
+
+export default function Products(){
+   
+    return(
+        <>
+            <div className="item1">
+                <h1>Flower Shop</h1>
+            </div>
+            <div className="item2">
+                <h4 className="card-title">Buy flowers</h4>
+                <div className="grid-container">
+                    {
+                        <Product/>
+                    }
+                </div>
+
+            </div>
+            <div className="item3">
+                {
+                //cart
+                }
+            </div>
+        </>
+    );
+
+}
